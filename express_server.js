275066134b0cac045fadf705f4a1fde85258a6ca
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-04 17:33:49 -0400

    added a post request for /urls

--- express_server.js
+++ express_server.js
@@ -6,6 +6,18 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+const generateRandomString = function() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
+
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -36,6 +48,11 @@ app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const templateVars = { id, longURL: urlDatabase[id]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
