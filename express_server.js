bfa0c80743e0fc04e40045ab17ebb017cd5c1950
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-04 18:25:20 -0400

    Made a post request that adds to the urlDatabase. And added redirects to the longURL

--- express_server.js
+++ express_server.js
@@ -51,8 +51,14 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
