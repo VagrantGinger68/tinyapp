bdb938db684c34c5f48e9d1032ce73e7548d08c4
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-05 18:47:06 -0400

    added username cookie to templateVars and added cookie-parser

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const cookieParser = require('cookie-parser');
+
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const generateRandomString = function() {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -34,17 +37,18 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urlDatabase };
+  const templateVars = { urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const templateVars = { id, longURL: urlDatabase[id]};
+  const templateVars = { id, longURL: urlDatabase[id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
