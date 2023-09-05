ecb4a9cec06f7f803e0a3a3f4370a9783237baf3
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-05 13:49:57 -0400

    added /urls/:id/delete route

--- express_server.js
+++ express_server.js
@@ -61,6 +61,13 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  console.log(`Deleted ${id} from urlDatabase`);
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
