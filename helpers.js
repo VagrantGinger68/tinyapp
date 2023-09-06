7d239433b630db903b995715435df92475b787a0
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-06 12:49:02 -0400

    moved generateRandomString to seperate file

---
+++ helpers.js
@@ -0,0 +1,15 @@
+const generateRandomString = function() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
+
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+};
+
+
+
+module.exports = { generateRandomString };
